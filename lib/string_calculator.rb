0dbc239ad49082fc7b02ac12bbaf52436c1a0134
Amit gupta <amit25gupta01@gmail.com>
2025-04-04 14:24:09 +0530

    Add code for string calculator logic

---
+++ lib/string_calculator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Class is string calculator.
+class StringCalculator
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter, numbers = extract_delimiter(numbers)
+    numbers_array = parse_numbers(numbers, delimiter)
+
+    validate_numbers(numbers_array)
+
+    numbers_array.sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    if numbers.start_with?('//')
+      delimiter = numbers[2]
+      numbers = numbers.split("\n", 2).last
+    else
+      delimiter = ','
+    end
+    [delimiter, numbers]
+  end
+
+  def parse_numbers(numbers, delimiter)
+    numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i)
+  end
+
+  def validate_numbers(numbers_array)
+    negative_numbers = numbers_array.select(&:negative?)
+    raise ArgumentError, "Negative numbers not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
+  end
+end
